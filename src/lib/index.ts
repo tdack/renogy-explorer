8bcaccc0d24704222226726aac938b8b993f3a6e
Troy Dack <troy@dack.com.au>
2026-05-17 07:19:59 +0930

    chore(lib): export components from src/lib/index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as DeviceCard } from './components/DeviceCard.svelte';
+export { default as DeviceDetails } from './components/DeviceDetails.svelte';
+export { default as DeviceParameters } from './components/DeviceParameters.svelte';
+export { default as SolarYieldChart } from './components/SolarYieldChart.svelte';
+export { default as AlarmsList } from './components/AlarmsList.svelte';
+export { default as YieldSummary } from './components/YieldSummary.svelte';
